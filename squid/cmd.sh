930cdfa73a7042995e15bf150eb60fb3a4a7ecc3
Tim Webster <tim.webster@gmail.com>
2017-06-17 18:10:23 +0100

    initial commit

---
+++ squid/cmd.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# start Squid process
+squid -f /data/squid.conf
+status=$?
+if [ $status -ne 0 ]; then
+  echo "Failed to start squid: $status"
+  exit $status
+fi
+
+SQUID_LOG=/var/log/squid/cache.log
+
+# Wait for log and tail for container output
+while /bin/true; do
+  if [ -f $SQUID_LOG ]; then
+    tail -f $SQUID_LOG
+    break
+  fi
+done
+
+# Keep container running.
+while /bin/true; do
+  sleep 60
+done
