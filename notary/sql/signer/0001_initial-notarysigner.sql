3b28a0809ed0e812a260b1e356ca0a0de15ccd4e
Tim Webster <tim.webster@gmail.com>
2017-11-03 21:09:49 +0000

    give signer UPDATE sql permission

--- notary/sql/signer/0001_initial-notarysigner.sql
+++ notary/sql/signer/0001_initial-notarysigner.sql
@@ -3,4 +3,4 @@ CREATE DATABASE IF NOT EXISTS `notarysigner`;
 CREATE USER "signer"@"%" IDENTIFIED BY "signer";
 
 GRANT
-	SELECT, INSERT, DELETE ON `notarysigner`.* TO "signer"@"%";
+	SELECT, INSERT, UPDATE, DELETE ON `notarysigner`.* TO "signer"@"%";
