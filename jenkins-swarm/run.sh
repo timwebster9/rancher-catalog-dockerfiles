f46a116762a11164a3917cb865a5d6023cd102db
Tim Webster <tim.webster@gmail.com>
2017-11-10 21:34:50 +0000

    add stuff

---
+++ jenkins-swarm/run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+SWARM_ARGS=""
+if [ -n "${JENKINS_USER}" ]; then
+    SWARM_ARGS="${SWARM_ARGS} -username ${JENKINS_USER}"
+fi
+
+if [ -n "${JENKINS_PASS}" ]; then
+    SWARM_ARGS="${SWARM_ARGS} -passwordEnvVariable JENKINS_PASS"
+fi
+
+if [ -n "${SWARM_EXECUTORS}" ]; then
+    SWARM_ARGS="${SWARM_ARGS} -executors ${SWARM_EXECUTORS}"
+fi
+
+exec java -jar /usr/share/jenkins/swarm-client-${SWARM_CLIENT_VERSION}.jar -fsroot /workspace ${SWARM_ARGS} \
+       -master http://jenkins-primary:${JENKINS_PORT:-8080} -disableSslVerification
